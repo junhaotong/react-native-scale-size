5b65b7d9f58f190aa8e3db138c4fe4cc2d13a7bd
jeremy <15869294164@163.com>
2018-07-30 21:22:43 +0800

    add d.ts

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+declare module 'react-native-scale-size' {
+    export default class ScaleSize {
+        static scaleText: (size: number) => number;
+        static scaleSize: (size: number) => number;
+    }
+}
+
